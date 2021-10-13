60b8a3fd50cbfd0f3a8a7d432a1f58a73d6217cf
Andrew <andrewedark@gmail.com>
2021-10-13 13:44:55 +0100

    Initalised repository

---
+++ src/test/java/com/example/applicants/ApplicantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.applicants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicantsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
