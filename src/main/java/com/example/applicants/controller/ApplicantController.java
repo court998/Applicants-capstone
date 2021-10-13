c301a7d7a388e68036da94843d91d07155eaecf7
Andrew <andrewedark@gmail.com>
2021-10-13 14:10:52 +0100

    Added service instance to controller class (dependency injection)

--- src/main/java/com/example/applicants/controller/ApplicantController.java
+++ src/main/java/com/example/applicants/controller/ApplicantController.java
@@ -1,4 +1,16 @@
 package com.example.applicants.controller;
 
+import com.example.applicants.service.ApplicantService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ApplicantController {
+
+    private final ApplicantService service;
+
+    public ApplicantController(ApplicantService service) {
+        this.service = service;
+    }
+
+
 }
--- src/main/java/com/example/applicants/service/ApplicantService.java
+++ src/main/java/com/example/applicants/service/ApplicantService.java
@@ -1,4 +1,9 @@
 package com.example.applicants.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ApplicantService {
+
+
 }
